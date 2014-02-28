8f720ee9df6cdb3e305d2b442ac67c98e6f6e90a
Michael Spear <github@bsd4breakfast.us>
2014-02-28 09:50:09 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require('config.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<title>Web JukeBox</title>
 	<link rel="stylesheet" href="<?= $_bootstrap_css ?>">
 	<link rel="stylesheet" href="style.css"
@@ -14,6 +14,15 @@ require('config.php');
 	<script src="script.js"></script>
 </head>
 <body>
-
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-10">
+				song name thing... maybe
+			</div>
+			<div class="col-xs-2">
+				vote count thing
+			</div>
+		</div>
+	</div>
 </body>
 </html>
