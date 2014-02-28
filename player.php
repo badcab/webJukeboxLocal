87d5e7a16a2f42a817dd46d1df16702b620bb6eb
Michael Spear <github@bsd4breakfast.us>
2014-02-28 10:46:34 -0600

    Update player.php

--- player.php
+++ player.php
@@ -2,5 +2,44 @@
 //should not be accecable by web server only cli used to actually play the music
 require('config.php');
 
-$_SESSION['song_bank'] = NULL;
+
+$o = $argv[1];
+
+if($o == '--play'){
+	$Player = new Player();
+	$song_bank = $Player->loadSession();
+	//add a fail safe in case not enogh stuff was returned
+	while(TRUE){
+		$Player->playNext($song_bank);//will return path of next song to be played
+		//will need a way to update $song_bank
+	}
+}
+
+if($o == '--first'){
+	//couple first dance
+}
+
+if($o == '--parent'){
+	//parents dance or something
+}
+
+if($o == '--other'){
+	//I think there was a third option
+}
+
+if($o == '--setup'){
+	$Song = new Song();
+	$Song->scanMusicDir();
+	
+	//add a few songs to the heat, queue 
+}
+
+if($o == '--help'){
+	//print out pi ip address and what options I can run
+}
+
+
+
+echo "\n";
+
 ?>
