b3534dc5bbb89a067b82308a65980f1347f373ee
Michael Spear <github@bsd4breakfast.us>
2014-02-28 08:19:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 require('config.php');
 
 ?>
-
+<!DOCTYPE html>
 <html>
+<head>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<title>Web JukeBox</title>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.0.min.js"></script>
+	<script src="script.js"></script>
+</head>
+<body>
 
+</body>
 </html>
